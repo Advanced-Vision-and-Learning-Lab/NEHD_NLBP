307c944c87a8216c8d6d15b7b5faa01dc6858cec
Salim Al Kharsa <61260042+SalimAlkharsa@users.noreply.github.com>
2024-02-20 18:37:39 -0600

    Update Compute_LBP.py

--- Utils/Compute_LBP.py
+++ Utils/Compute_LBP.py
@@ -8,14 +8,12 @@ import torch.types
 import torch.nn as nn
 import numpy as np
 from skimage.feature import local_binary_pattern
-from torchvision import transforms
 import dask
 import warnings
-import pdb
     
 class LocalBinaryLayer(nn.Module):
     def __init__(self, in_channels, radius = 1, n_points = 8, method = 'default',
-                 num_bins = 8, density = True, device = 'cpu'):
+                 num_bins = 8, density = True, return_encoding = False, device = 'cpu'):
         super(LocalBinaryLayer, self).__init__()
         self.in_channels = in_channels
         self.radius = radius
@@ -25,6 +23,7 @@ class LocalBinaryLayer(nn.Module):
         self.density = density
         self.LBP_max = 2 ** (self.n_points) - 1
         self.device = device
+        self.return_encoding = return_encoding
     
     @dask.delayed
     def LBP(self,x):
@@ -36,17 +35,22 @@ class LocalBinaryLayer(nn.Module):
     
     @dask.delayed
     def histogram(self,x):
-        hist = []
-        for channel in range(0,x.shape[0]):
-            temp_hist = np.histogram(x[channel],bins=self.num_bins,density=self.density,
-                                range=(0,self.LBP_max))
-            hist.append(temp_hist[0])
-        hist = np.stack(hist, axis=0)
+        
+        if self.return_encoding:
+            return x
+        else:
+            hist = []
+            for channel in range(0,x.shape[0]):
+                temp_hist = np.histogram(x[channel],bins=self.num_bins,density=self.density,
+                                    range=(0,self.LBP_max))
+                hist.append(temp_hist[0])
+            hist = np.stack(hist, axis=0)
+            
         return hist
         
-    def forward(self,x):    
+    def forward(self,x):
+       
         #Convert representation to integer to disable warning (look into disabling warning)
-        #May need to use float as fair comparison with method
         x_npy = x.cpu().numpy()
         
         batch, channel, dimx, dimy = x.shape
@@ -54,9 +58,8 @@ class LocalBinaryLayer(nn.Module):
         lbp_list = []
         
         #Compute LBP bit map and histogram representation
-        #Works currently for single channel (multiple channels will need additional for loop)
+        # Works currently for single channel (multiple channels will need additional for loop)
         for sample in range(0,batch):
-            # lbp_temparr = self.LBP(x_npy[sample][0])
             lbp_temparr = self.LBP(x_npy[sample])
             lbp_temparr = self.histogram(lbp_temparr)
             lbp_list.append(lbp_temparr)
@@ -66,8 +69,11 @@ class LocalBinaryLayer(nn.Module):
         lbp_newarr = np.asarray(lbp_list)
         lbp = torch.from_numpy(lbp_newarr)
         
+        #Change array from N x C x B to N x (CB) (match neural feature)
         #This will need to be updated if doing reconstruction
-        lbp = torch.flatten(lbp,start_dim=1)
+        if not(self.return_encoding):
+            lbp = torch.flatten(lbp,start_dim=1)
+            
         lbp = lbp.type(torch.float32).to(x.device)
         
         return lbp
